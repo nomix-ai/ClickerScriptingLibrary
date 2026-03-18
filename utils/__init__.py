2fc03dbadfaafdaf16d5c49f7ee8f21433669475
Timofey Ivanenko <timtin206@gmail.com>
2026-03-18 21:59:59 +0800

    Make imports relative

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
-from utils.clicker import Clicker
-from utils.agent import Agent
-from utils.recognition import Element, Screen, get_screen
+from .clicker import Clicker
+from .agent import Agent
+from .recognition import Element, Screen, get_screen
 
 __all__ = ["Clicker", "Agent", "Element", "Screen", "get_screen"]
